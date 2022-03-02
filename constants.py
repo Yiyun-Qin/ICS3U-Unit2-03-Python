cddb879a3c3211b483467baa51049657291776dc
Yiyun Qin <yiyun.qin@mths.ca>
2022-03-02 19:51:57 +0000

    math program

---
+++ constants.py
@@ -0,0 +1,4 @@
+# constant definition
+# this module is called "constants" and has following lines of code:
+
+TAU = 6.28
---
+++ math.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Created by Yiyun Qin
+# Created on March 2022
+# This is the math program, with proper style
+
+import constants
+
+
+def main():
+    # This function calculates the circumference of a circle
+    constants.TAU
+
+    # input
+    radius = int(input("Enter the length of the radius (mm): "))
+
+    # process
+    circumference = constants.TAU * radius
+
+    # output
+    print("")
+    print("Circumference is {} mm.".format(circumference))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
